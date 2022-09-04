6b7fd1fa24e1e3b120a98d2551035bb0d24702bc
jackrickman <jack@therickmans.com>
2022-09-04 13:04:05 +0100

    change to result variable

--- main.py
+++ main.py
@@ -1,35 +1,99 @@
 import random
 
-difficulties = ['easy', 'normal', 'hard']
+difficulties = ['very easy', 'easy', 'normal', 'hard']
+
+valid_difficulty = False
 
 difficulty = ''
 correct_number = ''
 guess_number = 0
+maximum_guesses = ''
+guess_length = ''
 
 print("""
 Welcome to the Mastermind Game!!
 Your task is to try and guess the correct number. You are told on guessing how many of your digits are the same
 but not the positions they are in.
 Difficulties:
+    Very Easy: Number length = 3 (you are told where the correct digits are)
     Easy: Number length = 2
     Normal: Number length = 3
     Hard: Number length = 3 (guesses are limited to 7!)""")
 
+# Checks if input difficulty is valid.
+
 while not valid_difficulty:
-    valid_difficulty = False
-    
-    difficulty = input('Difficulty: ')
+    difficulty = input('\nDifficulty: ').lower()
     if difficulty in difficulties:
         valid_difficulty = True
     else:
         print('Please try again, that is not a valid difficulty')
 
 
+# Selects a random number based on the parameters given in difficulty selection.
+
 def random_number_selection(upper_bound):
     random_number = random.randint(1, upper_bound)
     return random_number
 
 
+# Iterates over the player guess and the correct number, returning the number of matching indexes as the positions
+# variable, then formats an output according to its length.
+
+def easy_game():
+    def check_easy_guess():
+        i = 0
+        positions = ''
+        while i < guess_length:
+            if str(easy_game_guess)[i] == str(correct_number)[i]:
+                positions += str(i + 1)
+            i += 1
+        return positions
+
+    check_easy_guess()
+    correct_digit_positions = check_easy_guess()
+
+    length = len(correct_digit_positions)
+
+    if length == 0:
+        return 'There are not correct digits.'
+    elif length == 1:
+        return f'Correct digit location: {correct_digit_positions}'
+    elif length == 2:
+        return f'Correct digit location: {correct_digit_positions[0]} and {correct_digit_positions[1]}'
+    else:
+        return True
+
+
+# Visual output for the player telling them how many guesses they took and what the correct number was.
+
+def winning_the_game():
+    print(f"""
+    Well Done! You guessed the number in {guess_number} guesses.
+    The correct number was {correct_number}.""")
+
+
+def losing_the_game():
+    print(f"""
+    Stop right there! You have exceeded the maximum guess count. The correct number was {correct_number}.""")
+
+
+# Checks if guess is valid (i.e. suits the guess length and contains no other characters).
+
+def check_guess():
+    while True:
+        guess = input('Guess: ')
+        if len(guess) == guess_length:
+            return guess
+        print('That is not a valid guess, please try again...')
+
+
+# Checks the difficulty chosen and sets the parameters for game functions.
+
+if difficulty == 'very easy':
+    correct_number = random_number_selection(1000)
+    maximum_guesses = 999
+    guess_length = 3
 if difficulty == 'easy':
     correct_number = random_number_selection(100)
     maximum_guesses = 999
@@ -43,36 +107,18 @@ if difficulty == 'hard':
     maximum_guesses = 7
     guess_length = 3
 
-
-def check_guess():
-    correct_guess = False
-
-    while not correct_guess:
-        guess = input('Guess: ')
-        if len(guess) == guess_length:
-            correct_guess = True
-        else:
-            print('That is not a valid guess, please try again...')
-    return guess
+print(correct_number)
 
 
-def winning_the_game():
-    print("""
-    Well Done! You guessed the number in {} guesses.
-    The correct number was {}.""".format(guess_number, correct_number))
-
-
-def losing_the_game():
-    print("""
-    Stop right there! You have exceeded the maximum guess count. The correct number was {}.""".format(correct_number))
-
+# Sets a result variable to 0 and iterates over both the player's guess and the correct number, returning the
+# number of common digits.
 
 def checking_correct_digits():
     i = 0
     result = 0
-    guess = check_guess()
+    normal_mode_guess = check_guess()
     while i <= guess_length - 1:
-        value2 = str(guess)[i]
+        value2 = str(normal_mode_guess)[i]
         value1 = str(correct_number)[i]
         i += 1
         if value1 == value2:
@@ -80,15 +126,29 @@ def checking_correct_digits():
     return result
 
 
+# Checks whether maximum guesses has been exceeded. If not, the common digit variable is printed however if it is,
+# either the winning game or losing game function is called.
+
 while guess_number <= maximum_guesses:
     guess_number += 1
-    result = checking_correct_digits()
-    if correct_digits == guess_length:
-        winning_the_game()
-        break
-    elif guess_number > maximum_guesses:
-        losing_the_game()
-        break
+    if difficulty == 'very easy':
+        easy_game_guess = check_guess()
+        print(easy_game())
+
+        if easy_game():
+            winning_the_game()
+            break
+
     else:
-        print("Common digits: {}.".format(result))
-        continue
+        common_digits = checking_correct_digits()
+        if common_digits == guess_length:
+            winning_the_game()
+            break
+
+        elif guess_number > maximum_guesses:
+            losing_the_game()
+            break
+
+        else:
+            print(f"Common digits: {common_digits}.")
+            continue
